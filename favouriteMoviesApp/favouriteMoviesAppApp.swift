52f5ba5d305659f2ec19bd568cd3979e009f4069
cyrus chen <pschen@lcs.on.ca>
2023-05-27 10:48:17 -0400

    Initial Commit

---
+++ favouriteMoviesApp/favouriteMoviesAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  favouriteMoviesAppApp.swift
+//  favouriteMoviesApp
+//
+//  Created by Cyrus Chen on 27/5/2023.
+//
+
+import SwiftUI
+
+@main
+struct favouriteMoviesAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
