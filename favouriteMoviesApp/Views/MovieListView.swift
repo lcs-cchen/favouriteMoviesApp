fa39cb0433466b441de0ca45736e7b03d81c46c6
cyrus chen <pschen@lcs.on.ca>
2023-05-28 22:03:32 -0400

    Can add a movie from within a sheet.

--- favouriteMoviesApp/Views/MovieListView.swift
+++ favouriteMoviesApp/Views/MovieListView.swift
@@ -13,12 +13,25 @@ struct MovieListView: View {
         try await Movie.read(from: db)
     }) var movies
     
+    @State var showingAddMovieView = false
+    
     var body: some View {
         NavigationView{
             List(movies.results){ currentMovie in
                 MovieItemView(name: currentMovie.name, genre: currentMovie.genre, rating: currentMovie.rating)
             }
             .navigationTitle("Favorite Movie")
+            .toolbar{
+                ToolbarItem(placement: .primaryAction){
+                    Button(action: {
+                        showingAddMovieView = true
+                    }, label:{ Image(systemName: "plus")})
+                    .sheet(isPresented: $showingAddMovieView){
+                        AddMovieView()
+                            .presentationDetents([.fraction(0.3)])
+                    }
+                }
+            }
         }
                     
             }
